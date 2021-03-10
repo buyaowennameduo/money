82cb2cce94e411ec47921e66d3b37b98a80e1179
18210207783@163.com <18210207783@163.com>
2021-03-10 09:57:21 +0800

    2021.03.10

--- src/main/java/com/wq/money/system/controller/SysUserController.java
+++ src/main/java/com/wq/money/system/controller/SysUserController.java
@@ -1,19 +1,23 @@
-package com.wq.money.framework.controller;
+package com.wq.money.system.controller;
 
+import com.wq.money.system.service.SysUserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Api(value = "测试", tags = {"测试"})
-@Controller
-public class UserController {
+import javax.annotation.Resource;
 
-    private final Logger log = LoggerFactory.getLogger(UserController.class);
-    private Logger log1;
+@Api(value = "用户管理", tags = {"用户管理"})
+@Controller
+@RequestMapping(value = "sysUser")
+public class SysUserController {
+
+    private final Logger log = LoggerFactory.getLogger(SysUserController.class);
 
     @ApiOperation(value = "测试页面", notes = "测试页面")
     @GetMapping("index")
@@ -24,10 +28,13 @@ public class UserController {
         log.error("--error");
         return "index.html";
     }
+
     @ApiOperation(value = "测试数据", notes = "测试数据")
     @GetMapping("getStr")
     @ResponseBody
     public String getStr(){
         return "getStr";
     }
+
+
 }
